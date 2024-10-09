76a6653388d5b0a1b8d68d5916e35af20ba1bafc
Antonlvanov <156616115+Antonlvanov@users.noreply.github.com>
2024-10-09 11:43:40 +0300

    Add files via upload

---
+++ Pildi_vaatamine.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elemendid_vormis_TARpv23
+{
+    public partial class Pildi_vaatamine : Form
+    {
+        public Pildi_vaatamine(int w, int h)
+        {
+            this.Height = h;
+            this.Width = w;
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundColorButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Stretch_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Piltide_leidmine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elemendid_vormis_TARpv23
+{
+    public partial class Piltide_leidmine : Form
+    {
+        public Piltide_leidmine(int w, int h)
+        {
+            this.Height = h;
+            this.Width = w;
+        }
+    }
+}
