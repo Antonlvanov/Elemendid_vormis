1c31523d529055efeb50ced7ca2dd7b7ff9392e9
Anton Ivanov <anton9032@gmail.com>
2024-10-20 20:28:32 +0300

    aasdw

--- Araarvamismang.cs
+++ Araarvamismang.cs
@@ -7,99 +7,146 @@ namespace Elemendid_vormis_TARpv23
         private Label plusLeftLabel, plusRightLabel, minusLeftLabel, minusRightLabel, plusSign, minusSign;
         private Label timesLeftLabel, timesRightLabel, dividedLeftLabel, dividedRightLabel, timesSign, dividedSign;
         private Label timeLeftText;
-        private NumericUpDown sum, difference, product, quotient;
+        private NumericUpDown sumAnswer, minusAnswer, timesAnswer, devideAnswer;
         private Button startButton;
         private System.Windows.Forms.Timer timer1;
         Random randomizer = new Random();
+
+        private Button checkAnswersButton;
+
+        private Label correctAnswersLabel, wrongAnswersLabel;
+        private int correctAnswers = 0;
+        private int wrongAnswers = 0;
 
         int addend1, addend2;
         int minuend, subtrahend;
         int multiplicand, multiplier;
         int dividend, divisor;
         int timeLeft;
+        int difficulty = 1;
+        int baseTime = 30;
+        int additionalTime = 10;
 
         public Araarvamismang()
         {
-            this.Width = 550;
+            this.Width = 500;
             this.Height = 450;
             this.FormBorderStyle = FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
+            this.BackColor = Color.LightGray;
 
+            startButton = new Button();
             timer1 = new System.Windows.Forms.Timer(); // timer1
+            sumAnswer = new NumericUpDown();
+            minusAnswer = new NumericUpDown();
+            timesAnswer = new NumericUpDown();
+            devideAnswer = new NumericUpDown();
+            Panel linePanel = new Panel();
+            checkAnswersButton = new Button();
+
+            // timer
             timer1.Interval = 1000;
             timer1.Tick += new EventHandler(timer1_Tick);
 
-            sum = new NumericUpDown();
-            difference = new NumericUpDown();
-            product = new NumericUpDown();
-            quotient = new NumericUpDown();
-            startButton = new Button();
+            // answer count labels
+            correctAnswersLabel = CreateLabel("Correct Answers: 0", new Font("Arial", 12), new Size(200, 30), new Point(10, 10), true);
+            wrongAnswersLabel = CreateLabel("Wrong Answers: 0", new Font("Arial", 12), new Size(200, 30), new Point(10, 30), true);
+            correctAnswersLabel.ForeColor = Color.Green;
+            wrongAnswersLabel.ForeColor = Color.Red;
 
-            // init elements
-            timeLabel = CreateLabel("", new Font("Arial", 15.75f), new Size(200, 30), new Point(300, 0), false);
-            timeLeftText = CreateLabel("Time Left", new Font("Arial", 15.75f), new Size(100, 30), new Point(50, 0), true);
+            // line
+            linePanel.Size = new Size(this.Width, 2);
+            linePanel.Location = new Point(0, 50);
+            linePanel.BackColor = Color.Black;
+
+            // tags
+            timeLabel = CreateLabel("", new Font("Arial", 15.75f), new Size(50, 30), new Point(265, 7), false);
+            timeLabel.ForeColor = Color.Red;
+            timeLeftText = CreateLabel("Time Left:", new Font("Arial", 15.75f), new Size(70, 30), new Point(170, 10), true);
 
             plusLeftLabel = CreateLabel("?", new Font("Arial", 18), new Size(60, 50), new Point(50, 75), true);
-            plusRightLabel = CreateLabel("?", new Font("Arial", 18), new Size(60, 50), new Point(200, 75), true);
+            plusRightLabel = CreateLabel("?", new Font("Arial", 18), new Size(60, 50), new Point(190, 75), true);
             plusSign = CreateLabel("+", new Font("Arial", 18), new Size(30, 50), new Point(120, 75), true);
 
             minusLeftLabel = CreateLabel("?", new Font("Arial", 18), new Size(60, 50), new Point(50, 150), true);
-            minusRightLabel = CreateLabel("?", new Font("Arial", 18), new Size(60, 50), new Point(200, 150), true);
+            minusRightLabel = CreateLabel("?", new Font("Arial", 18), new Size(60, 50), new Point(190, 150), true);
             minusSign = CreateLabel("-", new Font("Arial", 18), new Size(30, 50), new Point(120, 150), true);
 
             timesLeftLabel = CreateLabel("?", new Font("Arial", 18), new Size(60, 50), new Point(50, 225), true);
-            timesRightLabel = CreateLabel("?", new Font("Arial", 18), new Size(60, 50), new Point(200, 225), true);
-            timesSign = CreateLabel("*", new Font("Arial", 18), new Size(30, 50), new Point(120, 225), true);
+            timesRightLabel = CreateLabel("?", new Font("Arial", 18), new Size(60, 50), new Point(190, 225), true);
+            timesSign = CreateLabel("×", new Font("Arial", 18), new Size(30, 50), new Point(120, 225), true);
 
             dividedLeftLabel = CreateLabel("?", new Font("Arial", 18), new Size(60, 50), new Point(50, 300), true);
-            dividedRightLabel = CreateLabel("?", new Font("Arial", 18), new Size(60, 50), new Point(200, 300), true);
-            dividedSign = CreateLabel("/", new Font("Arial", 18), new Size(30, 50), new Point(120, 300), true);
+            dividedRightLabel = CreateLabel("?", new Font("Arial", 18), new Size(60, 50), new Point(190, 300), true);
+            dividedSign = CreateLabel(":", new Font("Arial", 18), new Size(30, 50), new Point(120, 300), true);
 
-            // =
-            Controls.Add(CreateLabel("=", new Font("Arial", 18), new Size(30, 50), new Point(300, 75), false));
-            Controls.Add(CreateLabel("=", new Font("Arial", 18), new Size(30, 50), new Point(300, 150), false));
-            Controls.Add(CreateLabel("=", new Font("Arial", 18), new Size(30, 50), new Point(300, 225), false));
-            Controls.Add(CreateLabel("=", new Font("Arial", 18), new Size(30, 50), new Point(300, 300), false));
+            Controls.Add(CreateLabel("=", new Font("Arial", 18), new Size(30, 50), new Point(260, 60), false));
+            Controls.Add(CreateLabel("=", new Font("Arial", 18), new Size(30, 50), new Point(260, 140), false));
+            Controls.Add(CreateLabel("=", new Font("Arial", 18), new Size(30, 50), new Point(260, 210), false));
+            Controls.Add(CreateLabel("=", new Font("Arial", 18), new Size(30, 50), new Point(260, 285), false));
 
-            // events
+            // NumericUpDown
+            sumAnswer.Font = new Font(sumAnswer.Font.FontFamily, 18);
+            sumAnswer.MaximumSize = new Size(100, 0);
+            sumAnswer.Location = new Point(330, 65);
+            sumAnswer.Enter += new EventHandler(answer_Enter);
 
-            sum.Font = new Font(sum.Font.FontFamily, 18);
-            sum.MaximumSize = new Size(100, 0);
-            sum.Location = new Point(350, 75);
-            sum.Enter += new EventHandler(answer_Enter);
+            minusAnswer.Font = new Font(minusAnswer.Font.FontFamily, 18);
+            minusAnswer.MaximumSize = new Size(100, 0);
+            minusAnswer.Location = new Point(330, 145);
+            minusAnswer.Enter += new EventHandler(answer_Enter);
 
-            difference.Font = new Font(difference.Font.FontFamily, 18);
-            difference.MaximumSize = new Size(100, 0);
-            difference.Location = new Point(350, 150);
-            difference.Enter += new EventHandler(answer_Enter);
+            timesAnswer.Font = new Font(timesAnswer.Font.FontFamily, 18);
+            timesAnswer.MaximumSize = new Size(100, 0);
+            timesAnswer.Location = new Point(330, 215);
+            timesAnswer.Enter += new EventHandler(answer_Enter);
 
-            product.Font = new Font(product.Font.FontFamily, 18);
-            product.MaximumSize = new Size(100, 0);
-            product.Location = new Point(350, 225);
-            product.Enter += new EventHandler(answer_Enter);
+            devideAnswer.Font = new Font(devideAnswer.Font.FontFamily, 18);
+            devideAnswer.MaximumSize = new Size(100, 0);
+            devideAnswer.Location = new Point(330, 290);
+            devideAnswer.Enter += new EventHandler(answer_Enter);
 
-            quotient.Font = new Font(quotient.Font.FontFamily, 18);
-            quotient.MaximumSize = new Size(100, 0);
-            quotient.Location = new Point(350, 300);
-            quotient.Enter += new EventHandler(answer_Enter);
+            sumAnswer.Minimum = int.MinValue;
+            sumAnswer.Maximum = int.MaxValue;
+            minusAnswer.Minimum = int.MinValue;
+            minusAnswer.Maximum = int.MaxValue;
+            timesAnswer.Minimum = int.MinValue;
+            timesAnswer.Maximum = int.MaxValue;
+            devideAnswer.Minimum = int.MinValue;
+            devideAnswer.Maximum = int.MaxValue;
 
+            // buttons
             startButton.Text = "Start the quiz";
             startButton.Font = new Font(startButton.Font.FontFamily, 14);
             startButton.AutoSize = true;
-            startButton.Location = new Point(200, 375);
+            startButton.Location = new Point(190, 360);
             startButton.Click += new EventHandler(StartButton_Click);
 
-            // controls
-            Controls.Add(sum);
-            Controls.Add(difference);
-            Controls.Add(product);
-            Controls.Add(quotient);
-            Controls.Add(startButton);
+            checkAnswersButton.Text = "Check Answers";
+            checkAnswersButton.Font = new Font(checkAnswersButton.Font.FontFamily, 14);
+            checkAnswersButton.AutoSize = true;
+            checkAnswersButton.Location = new Point(30, 360);
+            checkAnswersButton.Click += new EventHandler(CheckAnswersButton_Click);
 
+            // adding controls
+            Controls.Add(sumAnswer);
+            Controls.Add(minusAnswer);
+            Controls.Add(timesAnswer);
+            Controls.Add(devideAnswer);
+            Controls.Add(startButton);
+            Controls.Add(checkAnswersButton);
             Controls.Add(plusSign);
             Controls.Add(minusSign);
             Controls.Add(timesSign);
             Controls.Add(dividedSign);
+            Controls.Add(linePanel);
+            Controls.Add(correctAnswersLabel);
+            Controls.Add(wrongAnswersLabel);
+        }
+
+        private void CheckAnswersButton_Click(object? sender, EventArgs e)
+        {
+            CheckAnswers();
         }
 
         private Label CreateLabel(string text, Font font, Size size, Point location, bool autoSize)
@@ -125,83 +172,78 @@ namespace Elemendid_vormis_TARpv23
 
         public void StartTheQuiz()
         {
-            // Fill in the addition problem.
-            // Generate two random numbers to add.
-            // Store the values in the variables 'addend1' and 'addend2'.
-            addend1 = randomizer.Next(51);
-            addend2 = randomizer.Next(51);
+            // reset
+            sumAnswer.BackColor = SystemColors.Window; 
+            minusAnswer.BackColor = SystemColors.Window;
+            timesAnswer.BackColor = SystemColors.Window;
+            devideAnswer.BackColor = SystemColors.Window;
+            sumAnswer.Value = 0;
+            minusAnswer.Value = 0; 
+            timesAnswer.Value = 0;
+            devideAnswer.Value = 0;
 
-            // Convert the two randomly generated numbers
-            // into strings so that they can be displayed
-            // in the label controls.
+            // make numbers 
+
+            addend1 = randomizer.Next(5 * difficulty, 101 * difficulty); 
+            addend2 = randomizer.Next(5 * difficulty, 101 * difficulty);
+
+            minuend = randomizer.Next(10 * difficulty, 101 * difficulty); 
+            subtrahend = randomizer.Next(1, minuend + 1);
+
+            do
+            {
+                multiplicand = randomizer.Next(5 * difficulty, 11 * difficulty);
+                multiplier = randomizer.Next(5 * difficulty, 11 * difficulty);
+            } while (!((multiplicand < 10) || (multiplicand % 5 == 0) || (multiplier < 10) || (multiplier % 5 == 0)));
+
+            do
+            {
+                divisor = randomizer.Next(3 * difficulty, 11 * difficulty);
+            } while (!(divisor < 10 || divisor % 5 == 0));
+
+            int temporaryQuotient = randomizer.Next(1, 11 * difficulty); 
+            dividend = divisor * temporaryQuotient;
+
+            // add numbers to labels
             plusLeftLabel.Text = addend1.ToString();
             plusRightLabel.Text = addend2.ToString();
-
-            // 'sum' is the name of the NumericUpDown control.
-            // This step makes sure its value is zero before
-            // adding any values to it.
-            sum.Value = 0;
-
-            // Fill in the subtraction problem.
-            minuend = randomizer.Next(1, 101);
-            subtrahend = randomizer.Next(1, minuend);
             minusLeftLabel.Text = minuend.ToString();
             minusRightLabel.Text = subtrahend.ToString();
-            difference.Value = 0;
-
-            // Fill in the multiplication problem.
-            multiplicand = randomizer.Next(2, 11);
-            multiplier = randomizer.Next(2, 11);
             timesLeftLabel.Text = multiplicand.ToString();
             timesRightLabel.Text = multiplier.ToString();
-            product.Value = 0;
-
-            // Fill in the division problem.
-            divisor = randomizer.Next(2, 11);
-            int temporaryQuotient = randomizer.Next(2, 11);
-            dividend = divisor * temporaryQuotient;
             dividedLeftLabel.Text = dividend.ToString();
             dividedRightLabel.Text = divisor.ToString();
-            quotient.Value = 0;
+            
+            // reset answers
+            timesAnswer.Value = 0;
+            sumAnswer.Value = 0;
+            timesAnswer.Value = 0;
+            devideAnswer.Value = 0;
 
             // Start the timer.
-            timeLeft = 30;
-            timeLabel.Text = "30 seconds";
+            timeLeft = baseTime + (additionalTime * (difficulty - 1));
+            timeLabel.Text = timeLeft.ToString();
             timer1.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (CheckTheAnswer())
+            if (timeLeft > 0)
             {
-                // If CheckTheAnswer() returns true, then the user 
-                // got the answer right. Stop the timer  
-                // and show a MessageBox.
-                timer1.Stop();
-                MessageBox.Show("You got all the answers right!",
-                                "Congratulations!");
-                startButton.Enabled = true;
-            }
-            else if (timeLeft > 0)
-            {
-                // If CheckTheAnswer() returns false, keep counting
-                // down. Decrease the time left by one second and 
-                // display the new time left by updating the 
-                // Time Left label.
                 timeLeft = timeLeft - 1;
-                timeLabel.Text = timeLeft + " seconds";
+                timeLabel.Text = timeLeft + "";
             }
             else
             {
-                // If the user ran out of time, stop the timer, show
-                // a MessageBox, and fill in the answers.
-                timer1.Stop();
                 timeLabel.Text = "Time's up!";
-                MessageBox.Show("You didn't finish in time.", "Sorry!");
-                sum.Value = addend1 + addend2;
-                difference.Value = minuend - subtrahend;
-                product.Value = multiplicand * multiplier;
-                quotient.Value = dividend / divisor;
+                sumAnswer.Value = addend1 + addend2;
+                minusAnswer.Value = minuend - subtrahend;
+                timesAnswer.Value = multiplicand * multiplier;
+                devideAnswer.Value = dividend / divisor;
+
+                CheckAnswers();
+                difficulty += 1;
+                startButton.Text = "Start level " + difficulty.ToString();
                 startButton.Enabled = true;
             }
         }
@@ -218,15 +260,59 @@ namespace Elemendid_vormis_TARpv23
             }
         }
 
-        private bool CheckTheAnswer()
+        private void CheckAnswers()
         {
-            if ((addend1 + addend2 == sum.Value)
-                && (minuend - subtrahend == difference.Value)
-                && (multiplicand * multiplier == product.Value)
-                && (dividend / divisor == quotient.Value))
-                return true;
+            timer1.Stop();
+            CountPoints();
+
+            if ((addend1 + addend2 == sumAnswer.Value)
+                && (minuend - subtrahend == minusAnswer.Value)
+                && (multiplicand * multiplier == timesAnswer.Value)
+                && (dividend / divisor == devideAnswer.Value))
+            {
+                startButton.Enabled = true;
+                difficulty += 1; // increase difficulty
+                startButton.Text = "Start level " + difficulty.ToString();
+            }
             else
-                return false;
+            {
+                sumAnswer.Value = addend1 + addend2;
+                minusAnswer.Value = minuend - subtrahend;
+                timesAnswer.Value = multiplicand * multiplier;
+                devideAnswer.Value = dividend / divisor;
+                difficulty += 1;
+                startButton.Text = "Start level " + difficulty.ToString();
+                startButton.Enabled = true;
+            }
+        }
+
+        private void CountPoints()
+        {
+            correctAnswers += (addend1 + addend2 == sumAnswer.Value) ? (sumAnswer.BackColor = Color.Green, 1).Item2 : (sumAnswer.BackColor = Color.Red, 0).Item2;
+            wrongAnswers += (addend1 + addend2 == sumAnswer.Value) ? 0 : 1;
+
+            correctAnswers += (minuend - subtrahend == minusAnswer.Value) ? (minusAnswer.BackColor = Color.Green, 1).Item2 : (minusAnswer.BackColor = Color.Red, 0).Item2;
+            wrongAnswers += (minuend - subtrahend == minusAnswer.Value) ? 0 : 1;
+
+            correctAnswers += (multiplicand * multiplier == timesAnswer.Value) ? (timesAnswer.BackColor = Color.Green, 1).Item2 : (timesAnswer.BackColor = Color.Red, 0).Item2;
+            wrongAnswers += (multiplicand * multiplier == timesAnswer.Value) ? 0 : 1;
+
+            correctAnswers += (dividend / divisor == devideAnswer.Value) ? (devideAnswer.BackColor = Color.Green, 1).Item2 : (devideAnswer.BackColor = Color.Red, 0).Item2;
+            wrongAnswers += (dividend / divisor == devideAnswer.Value) ? 0 : 1;
+
+            correctAnswersLabel.Text = $"Correct Answers: {correctAnswers}";
+            wrongAnswersLabel.Text = $"Wrong Answers: {wrongAnswers}";
+
+            // colors
+            sumAnswer.BackColor = Color.Red;
+            minusAnswer.BackColor = Color.Red;
+            timesAnswer.BackColor = Color.Red;
+            devideAnswer.BackColor = Color.Red;
+
+            if (addend1 + addend2 == sumAnswer.Value) { sumAnswer.BackColor = Color.Green; }
+            if (minuend - subtrahend == minusAnswer.Value) { minusAnswer.BackColor = Color.Green; }
+            if (multiplicand * multiplier == timesAnswer.Value) { timesAnswer.BackColor = Color.Green; }
+            if (dividend / divisor == devideAnswer.Value) { devideAnswer.BackColor = Color.Green; }
         }
     }
 }
--- Piltide_leidmine.cs
+++ Piltide_leidmine.cs
@@ -7,6 +7,7 @@
         private Label timeLabel;
         private System.Windows.Forms.Timer timer;
         private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Timer showIconsTimer; // таймер на сокрытие иконок
         private Label firstClicked = null;
         private Label secondClicked = null;
         private int timeElapsed;
@@ -15,6 +16,13 @@
             "c", "c", "e", "e", "N", "N", "f", "f",
             "l", "l", "u", "u", "x", "x", "v", "v"
         };
+        // lives
+        private TableLayoutPanel topPanel;
+        private Label livesLabel;
+        private int lives = 5;
+
+        private System.Windows.Forms.Timer countdownTimer; // таймер для обратного отсчета
+        private int countdownValue; // начальное значение для обратного отсчета
 
         public Piltide_leidmine()
         {
@@ -22,13 +30,20 @@
             this.Height = 550;
             this.Text = "Matching Game";
 
-            
             mainLayoutPanel = new TableLayoutPanel();
             mainLayoutPanel.Dock = DockStyle.Fill;
             mainLayoutPanel.RowCount = 2;
             mainLayoutPanel.ColumnCount = 1;
             mainLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F)); 
-            mainLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F)); 
+            mainLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+
+            topPanel = new TableLayoutPanel(); // panel for top labels
+            topPanel.ColumnCount = 3; // Теперь 3 колонки: одна для отступа слева, вторая для времени, третья для жизней
+            topPanel.RowCount = 1;
+            topPanel.Dock = DockStyle.Fill;
+            topPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F)); // 50% для отступа слева
+            topPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 200F)); // фиксированная ширина для лейбла времени
+            topPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F)); // 50% для жизней
 
             timeLabel = new Label();
             timeLabel.AutoSize = false;
@@ -36,20 +51,35 @@
             timeLabel.Width = 200;
             timeLabel.Height = 30;
             timeLabel.Font = new Font(timeLabel.Font.FontFamily, 15.75f);
-            timeLabel.Text = "Время: 00:00";
+            timeLabel.Text = "Time: 00:00";
             timeLabel.TextAlign = ContentAlignment.MiddleCenter;
-            timeLabel.Dock = DockStyle.Fill; 
+            timeLabel.Dock = DockStyle.Fill; // Это поможет заполнить отведенное место
+            timeLabel.Anchor = AnchorStyles.None; // Без привязки, чтобы разместить ровно по центру
 
             tableLayoutPanel = new TableLayoutPanel();
             tableLayoutPanel.Dock = DockStyle.Fill;
             tableLayoutPanel.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset;
             tableLayoutPanel.ColumnCount = 4;
             tableLayoutPanel.RowCount = 4;
+
             for (int i = 0; i < 4; i++)
             {
                 tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
                 tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
             }
+            // add lives label
+            livesLabel = new Label();
+            livesLabel.AutoSize = false;
+            livesLabel.BorderStyle = BorderStyle.FixedSingle;
+            livesLabel.Width = 100;
+            livesLabel.Height = 30;
+            livesLabel.Font = new Font(livesLabel.Font.FontFamily, 15.75f);
+            livesLabel.Text = "Lives: 5";
+            livesLabel.TextAlign = ContentAlignment.MiddleCenter;
+            livesLabel.Dock = DockStyle.Fill;
+            livesLabel.Anchor = AnchorStyles.None;
+
+            // timers
 
             timer = new System.Windows.Forms.Timer();
             timer.Interval = 750;
@@ -59,16 +89,78 @@
             gameTimer.Interval = 1000;
             gameTimer.Tick += GameTimer_Tick;
 
-            mainLayoutPanel.Controls.Add(timeLabel, 0, 0); 
+            showIconsTimer = new System.Windows.Forms.Timer(); 
+            showIconsTimer.Interval = 1000; 
+            showIconsTimer.Tick += ShowIconsTimer_Tick;
+
+
+            // fill main panel layout
+            topPanel.Controls.Add(new Label(), 0, 0); // Пустой элемент для отступа слева
+            topPanel.Controls.Add(timeLabel, 1, 0); // Лейбл времени по центру
+            topPanel.Controls.Add(livesLabel, 2, 0);
+            mainLayoutPanel.Controls.Add(topPanel, 0, 0);
             mainLayoutPanel.Controls.Add(tableLayoutPanel, 0, 1); 
 
             Controls.Add(mainLayoutPanel);
 
             AddLabelsToTable();
-
             AssignIconsToSquares();
 
-            StartGameTimer();
+            ShowAllIcons();
+            StartShowIconsTimerWithCountdown();
+        }
+
+        private void StartShowIconsTimerWithCountdown()
+        {
+            countdownValue = 10; // Устанавливаем 5 секунд для запоминания
+            timeLabel.ForeColor = Color.Red; // Цвет времени красный для обратного отсчета
+            timeLabel.Text = $"Start in: {countdownValue}";
+
+            // Запуск таймера для обратного отсчета
+            showIconsTimer.Start();
+        }
+
+        private void ShowIconsTimer_Tick(object sender, EventArgs e)
+        {
+            countdownValue--;
+            timeLabel.Text = $"Start in: {countdownValue}";
+
+            if (countdownValue == 0)
+            {
+                showIconsTimer.Stop(); // Остановить таймер показа иконок
+                timeLabel.ForeColor = Color.Black; // Вернуть обычный цвет
+                timeLabel.Text = "Time: 00:00";
+
+                // Скрыть все иконки
+                HideAllIcons();
+
+                // Запуск основного таймера игры
+                StartGameTimer();
+            }
+        }
+
+        private void HideAllIcons()
+        {
+            foreach (Control control in tableLayoutPanel.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                }
+            }
+        }
+
+        private void ShowAllIcons()
+        {
+            foreach (Control control in tableLayoutPanel.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    iconLabel.ForeColor = Color.Black;
+                }
+            }
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -86,7 +178,7 @@
         {
             timeElapsed++;
             TimeSpan time = TimeSpan.FromSeconds(timeElapsed);
-            timeLabel.Text = "Время: " + time.ToString(@"mm\:ss");
+            timeLabel.Text = "Time: " + time.ToString(@"mm\:ss");
         }
 
         private void StartGameTimer()
@@ -142,10 +234,10 @@
 
         private void Label_Click(object sender, EventArgs e)
         {
+            Label clickedLabel = sender as Label;
+
             if (firstClicked != null && secondClicked != null)
                 return;
-
-            Label clickedLabel = sender as Label;
 
             if (clickedLabel == null)
                 return;
@@ -162,6 +254,22 @@
             }
 
             secondClicked = clickedLabel;
+
+            if (firstClicked.Tag.ToString() != secondClicked.Tag.ToString())
+            {
+                lives--;
+                livesLabel.Text = $"Lives: {lives}";
+
+                if (lives == 0)
+                {
+                    StopGameTimer();
+                    MessageBox.Show("You Lost", "Game Over");
+                    Close();
+                    return;
+                }
+
+                timer.Start();
+            }
 
             CheckForWinner();
 
@@ -188,7 +296,7 @@
 
             StopGameTimer();
 
-            MessageBox.Show($"Вы победили! Ваше время: {timeLabel.Text}", "Поздравляем");
+            MessageBox.Show($"You won! Time: {timeLabel.Text}", "Congratulations");
             Close();
         }
     }
