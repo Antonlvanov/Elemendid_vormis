d1d116a4e4333bf6518006272d167b5fa79bc7f3
Anton Ivanov <anton9032@gmail.com>
2024-10-22 22:56:56 +0300

    as

--- MaluMang/GameHandler.cs
+++ MaluMang/GameHandler.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text; 
 using System.Threading.Tasks;
 
 namespace Elemendid_vormis_TARpv23.MaluMang
